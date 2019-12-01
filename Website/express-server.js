0d0101ebc42aaba512fcc0cc10b5b624d5f28787
Jason Tungay <jtungay@gmail.com>
2019-12-01 17:42:39 +0000

    Done some express; listitems, item/id and / wich returns the index page with all its dependancies. Also,going to /productImages/Sekiro.jpg will display the image. Package and procfile will hopefully make heroku work, the CD pipeline from my master branch.

---
+++ Website/express-server.js
@@ -0,0 +1,46 @@
+var mongoose = require("mongoose");
+
+var uri = "mongodb+srv://jtungay:Sekiro2019@cluster0-xwkkp.mongodb.net/test?retryWrites=true&w=majority";
+var port = 9000;
+
+// Schemas.js, so we can get the card and game models from there
+var schemas = require("./schemas");
+
+// Import Express and initialise the application.
+express = require("express");
+var app = express();
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
+
+app.use(express.static(__dirname));
+
+app.get("/listitems", function(request, response) {
+	// Find all items.
+	schemas.Item.find(function(err, items) {
+		// Set the response header to indicate JSON content
+		// and return the array of Card data.
+		response.setHeader("Content-Type", "application/json");
+		response.send(items);
+	});
+});
+
+app.get("/item/:id", function (request, response) {
+	schemas.Item.find({"_id": request.params.id}, function(err, item) {
+		response.setHeader("Content-Type", "application/json");
+		response.send(item);
+	});
+});
+
+app.get("/", function(request, response) {
+	response.render("index");
+});
+
+// Run the server.
+app.listen(port, function() {
+	mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then((test) => {
+		console.log("Connected to DB");
+	});
+	console.log("Listening on 9000...");
+})
